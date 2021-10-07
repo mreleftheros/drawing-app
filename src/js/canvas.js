9b77f29dece767eb919a0354dbdf5daa6491c87d
eleftheros <eleftheros_all@protonmail.com>
2021-10-07 10:23:11 +0300

    Add updateCoords method in canvas

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -9,13 +9,23 @@ class Canvas {
     ];
     this.brush = this.brushes[1].value;
     this.color = "#000";
+    this.coords;
+    this.isDrawing = false;
   }
   init() {
     this.canvas = document.getElementById("canvas");
-    this.ctx = canvas.getContext("2d");    
+    this.ctx = canvas.getContext("2d");
+
+    this.canvas.addEventListener("mousemove", e => this.updateCoords(e));
   }
   setBrush(size) {
     return this.brush = this.brushes.filter(brush => brush.name === size)[0].value;
+  }
+  setColor(color) {
+    return this.color = color;
+  }
+  updateCoords(e) {
+    return this.coords = {x: e.offsetX, y: e.offsetY};
   }
 }
 
