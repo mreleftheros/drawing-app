5d2e7b6b031c45bb73baafc1dcd1ce67956f7325
eleftheros <eleftheros_all@protonmail.com>
2021-10-07 11:30:54 +0300

    Add brush images for cursor

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -8,6 +8,7 @@ class Canvas {
       {name: "large", value: 15},
     ];
     this.brush = this.brushes[1].value;
+    this.size = "medium";
     this.color = "#000";
     this.coords;
     this.isDrawing = false;
@@ -16,13 +17,20 @@ class Canvas {
     this.canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
 
-    this.canvas.addEventListener("mousemove", e => this.updateCoords(e));
+    this.canvas.addEventListener("mousemove", e => this.handleMouseMove(e));
   }
   setBrush(size) {
+    this.size = size;
     return this.brush = this.brushes.filter(brush => brush.name === size)[0].value;
   }
   setColor(color) {
     return this.color = color;
+  }
+  handleMouseMove(e) {
+    this.canvas.style.cursor = `url(./assets/brush-${this.size}-${this.color}.png), auto`;
+    this.canvas.style.caretColor = this.color;
+
+    return this.updateCoords(e);
   }
   updateCoords(e) {
     return this.coords = {x: e.offsetX, y: e.offsetY};
