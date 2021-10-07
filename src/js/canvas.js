4637377b80b68e621eb73f5e7acdaa463a3b0714
eleftheros <eleftheros_all@protonmail.com>
2021-10-07 09:54:25 +0300

    Add selectBrush method in ui

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,12 +1,18 @@
+import ui from "./ui";
+
 class Canvas {
   constructor() {
-    this.canvas;
-    this.ctx;
-    this.canvasWidth;
-    this.canvasHeight;
+    this.brushes = [
+      {name: "small", value: 5},
+      {name: "medium", value: 10},
+      {name: "large", value: 15},
+    ];
+    this.brushSize;
   }
   init() {
     this.canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
   }
 }
+
+export default new Canvas();
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,14 +1,32 @@
+import canvas from "./canvas";
+
 class Ui {
   constructor() {
     this.toggleBtn = document.getElementById("toggleBtn");
     this.toolbox = document.getElementById("toolbox");
+    this.brushes = document.getElementById("brushes");
+    this.swatches = document.getElementById("swatches");
+    this.btns = document.getElementById("btns");
   }
   init() {
     this.toggleBtn.addEventListener("click", e => this.toggleToolbox(e));
+    this.brushes.addEventListener("click", e => this.selectBrush(e));
   }
   toggleToolbox(e) {
     e.target.firstElementChild.classList.toggle("enabled");
     this.toolbox.classList.toggle("active");
+  }
+  selectBrush(e) {
+    if (e.target.tagName !== "BUTTON" || e.target.classList.contains("active")) return; // check
+    
+    let size = e.target.getAttribute("data-size");
+    canvas.brushSize = size;
+
+    for (let brush of this.brushes.children) {
+      brush.classList.remove("active");
+    }
+
+    e.target.classList.add("active");
   }
 }
 
